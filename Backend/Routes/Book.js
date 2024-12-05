4b6adfe2a4daaf0d575265d7773d9ec4ecd5eaf9
Laura Polet <laurapplt@gmail.com>
2024-12-05 09:37:17 +0100

    fix errors

--- Backend/Routes/Book.js
+++ Backend/Routes/Book.js
@@ -1,30 +1,15 @@
 const express = require('express');
+const multer = require('../Middleware/Multer-config');
 const router = express.Router();
-const auth = require('../Middleware/Auth');
+const auth= require('../Middleware/Auth')
 const bookCtrl = require('../Controllers/Book');
-const { upload, optimizeImage } = require('../Middleware/Multer-config');
 
-// --- Books
-
-// Index
 router.get('/', bookCtrl.getAllBooks);
-router.get('/bestrating', bookCtrl.getBestRating);
-
-// Create
-router.post('/', auth, upload, optimizeImage, bookCtrl.createBook);
-
-// Read
-router.get('/:id', bookCtrl.getOneBook);
-
-// Update
-router.put('/:id', auth, upload, optimizeImage, bookCtrl.modifyBook);
-
-// Delete
+router.post('/', auth, multer, bookCtrl.createBook);
+//router.post('/:id/rating', auth, bookCtrl.rateBook);
+router.get('/bestrating', bookCtrl.bestRating);
+router.get('/:id',bookCtrl.getOneBook);
+//router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
-
-// --- Books -> Rating
-
-// Create Rating
-router.post('/:id/rating', auth, bookCtrl.createRating);
 
 module.exports = router;
